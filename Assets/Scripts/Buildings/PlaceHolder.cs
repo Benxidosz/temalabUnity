81b509839c83f138000374949fb12ae76d91e812
BalintCsala <balint.csala@ecosim.hu>
2021-12-13 17:41:10 +0100

    Placeholder update

--- Assets/Scripts/Buildings/PlaceHolder.cs
+++ Assets/Scripts/Buildings/PlaceHolder.cs
@@ -5,42 +5,52 @@ using UnityEngine.Serialization;
 
 namespace Buildings
 {
-    public class PlaceHolder : MonoBehaviour{
+    public class PlaceHolder : MonoBehaviour
+    {
         [SerializeField] private List<PlaceHolder> neighbours;
         [SerializeField] private PlaceHolderType type;
-        [FormerlySerializedAs("StartingObj")] [SerializeField] private GameObject startingObj;
+
+        [FormerlySerializedAs("StartingObj")] [SerializeField]
+        private GameObject startingObj;
+
         private List<GameObject> _buildings;
 
         public PlaceHolderType Type => type;
         public List<PlaceHolder> Neighbours => neighbours;
-        public Building MainBuilding{ get; private set; }
+        public Building MainBuilding { get; private set; }
 
         public PlayerController Player { get; private set; }
 
-        private void Start(){
+        private void Start()
+        {
             _buildings = new List<GameObject>();
             var tmpNew = Instantiate(startingObj, gameObject.transform.position, Quaternion.identity);
-            tmpNew.transform.parent = this.transform;
+            tmpNew.transform.parent = transform;
             _buildings.Add(tmpNew);
         }
 
-        public void PlaceNew(Building prefab, PlayerController player){
-            if (_buildings.Count > 1){
+        public void PlaceNew(Building prefab, PlayerController player)
+        {
+            if (_buildings.Count > 1)
+            {
                 Destroy(_buildings[1]);
                 _buildings.RemoveAt(1);
             }
+
             var tmpNew = Instantiate(prefab.Model, gameObject.transform.position, Quaternion.identity);
-            tmpNew.transform.parent = this.transform;
+            tmpNew.transform.parent = transform;
             tmpNew.GetComponent<NetworkObject>().Spawn();
             _buildings.Add(tmpNew);
             _buildings[0].SetActive(false);
             MainBuilding = prefab;
-            Player = Player;
+            Player = player;
         }
-        
     }
 
-    public enum PlaceHolderType{
-        Node, Edge, Tile
+    public enum PlaceHolderType
+    {
+        Node,
+        Edge,
+        Tile
     }
 }
